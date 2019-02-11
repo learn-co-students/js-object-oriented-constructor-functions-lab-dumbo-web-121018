fe9ff24defa359c8d6e94486991817c3fd4289fa
jisung594 <jisung594@gmail.com>
2019-02-10 20:17:19 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function Scooter(year,color,model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+// new Scooter();
+
+
+function Driver(name,age,experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+// new Driver();
+
+
+function PickupLocation(address,city) {
+  this.address = address
+  this.city = city
+}
+
+// new PickupLocation();
